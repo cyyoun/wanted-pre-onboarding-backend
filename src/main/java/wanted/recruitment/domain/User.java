023200e75a45260101ec709deef30c486e379a8b
cyyoun <cyyoun41@gmail.com>
2023-10-10 10:39:26 +0900

    Feat: MySQL wanted 데이터베이스 연결

--- src/main/java/wanted/recruitment/domain/User.java
+++ src/main/java/wanted/recruitment/domain/User.java
@@ -1,7 +1,13 @@
 package wanted.recruitment.domain;
 
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     long userId;
     String userName;
 
